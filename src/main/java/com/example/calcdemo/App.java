f7874b147dce30463b630c877d75cdb74a433908
Pavan Sai Chitikina <chitikinapavansai@gmail.com>
2019-07-10 12:33:39 +0530

    Main completely edited

--- src/main/java/com/example/calcdemo/App.java
+++ src/main/java/com/example/calcdemo/App.java
@@ -5,8 +5,32 @@ import java.util.Scanner;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		Arithmetic arithmetic = null;
+		System.out.println("Calculator:\n1. Adddition\n2.Substraction\n3. Multiplication\n4. Division\nEnter a number corresponding to operation");
+		int option = scanner.nextInt();
+		double op1, op2, result;
+		System.out.println("Enter the operands");
+		op1 = scanner.nextInt();
+		op2 = scanner.nextInt();
+		if (option==1)
+		{
+			arithmetic = new addition();
+		}
+		else if (option==2)
+		{
+			arithmetic = new Substraction();
+		}
+		else if (option==3)
+		{
+			arithmetic = new Multiplication();
+		}
+		else
+		{
+			arithmetic = new Division();
+		}
+		result = arithmetic.evaluate(op1, op2);
+		System.out.println("Result : "+result);
 	}
 }

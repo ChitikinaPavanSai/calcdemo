3c66d66fd7761ed134b81ad0d80e99b6de5fc07f
aditya kabra <a.aditya1246@gmail.com>
2019-07-10 11:53:01 +0530

    adiition class

---
+++ src/main/java/com/example/calcdemo/addition.java
@@ -0,0 +1,12 @@
+package com.example.calcdemo;
+
+public class addition implements Arithmetic {
+
+	public double evaluate(double op1, double op2) {
+		return op2+op1;
+		// TODO Auto-generated method stub
+		//return 0;
+	}
+	
+
+}

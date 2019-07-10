57c27c2bd57ac8cdda0a26d09a0078098d28d8a0
Pavan Sai Chitikina <chitikinapavansai@gmail.com>
2019-07-10 12:39:20 +0530

    Edited

--- src/main/java/com/example/calcdemo/App.java
+++ src/main/java/com/example/calcdemo/App.java
@@ -8,10 +8,10 @@ public class App {
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		Arithmetic arithmetic = null;
-		System.out.println("Calculator:\n1. Adddition\n2.Substraction\n3. Multiplication\n4. Division\nEnter a number corresponding to operation");
+		System.out.println("Calculator:\n1. Adddition\n2.Substraction\n3. Multiplication\n4. Division\nEnter a number corresponding to operation :");
 		int option = scanner.nextInt();
 		double op1, op2, result;
-		System.out.println("Enter the operands");
+		System.out.println("Enter the operands : ");
 		op1 = scanner.nextInt();
 		op2 = scanner.nextInt();
 		if (option==1)

5b8c88b00ef5eabbe8af8a895aa6f44f96773b8d
Pavan Sai Chitikina <chitikinapavansai@gmail.com>
2019-07-10 11:44:34 +0530

    Created Interface

--- src/main/java/com/example/calcdemo/App.java
+++ src/main/java/com/example/calcdemo/App.java
@@ -1,13 +1,12 @@
 package com.example.calcdemo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Scanner;
+
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		Scanner scanner = new Scanner(System.in);
+		Arithmetic arithmetic = null;
+	}
 }

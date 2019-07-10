d360ee90b9a02e13bb7235bed9eab89f70e4d485
pratibha kumari <pratibha.kumari@amdocs.com>
2019-07-10 11:54:58 +0530

    Added division file

---
+++ src/main/java/com/example/calcdemo/Division.java
@@ -0,0 +1,10 @@
+package com.example.calcdemo;
+
+public class Division implements Arithmetic{
+
+	public double evaluate(double op1, double op2) {
+		// TODO Auto-generated method stub
+		return op1/op2;
+	}
+
+}
